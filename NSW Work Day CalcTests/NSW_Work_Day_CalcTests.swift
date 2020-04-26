91cf8f49e8508a58d0c514887029911164024274
Raj <raj1992_patel@windowslive.com>
2020-04-26 13:02:33 +1200

    Initial Commit

---
+++ NSW Work Day CalcTests/NSW_Work_Day_CalcTests.swift
@@ -0,0 +1,34 @@
+//
+//  NSW_Work_Day_CalcTests.swift
+//  NSW Work Day CalcTests
+//
+//  Created by Raj Patel on 26/04/20.
+//  Copyright © 2020 Raj Patel. All rights reserved.
+//
+
+import XCTest
+@testable import NSW_Work_Day_Calc
+
+class NSW_Work_Day_CalcTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
